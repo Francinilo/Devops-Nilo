57349149a1ae21e9a6e64bde5d1e0a23c94303bb
francinilo <francinilo@outlook.com>
2022-04-14 14:47:37 -0300

    Meu Primeiro Commit Santa

---
+++ main.tf
@@ -0,0 +1,27 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources2"
+  location = "West Europe"
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example5" {
+  name     = "example-resources5"
+  location = "West Europe"
+}
